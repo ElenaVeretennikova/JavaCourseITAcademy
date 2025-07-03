acab7881dab67af4f2e89a5d80d0abc152f0d503
Elena <Lena@MacBook-Pro-.local>
2025-07-03 17:04:19 +0300

    credit03

---
+++ src/Credit/Unit03Ex03.java
@@ -0,0 +1,22 @@
+package Credit;
+
+import java.util.Random;
+
+public class Unit03Ex03 {
+
+    public static void main(String[] args) {
+        int secCount = new Random().nextInt(28801);
+        System.out.println("Количество секунд: " + secCount);
+        System.out.println(getHoursCount(secCount));
+    }
+
+    public static String getHoursCount(int secCount) {
+        int hoursCount = secCount / 3600;
+        String hourName = switch (hoursCount) {
+            case 1 -> "час";
+            case 2,3,4 -> "часа";
+            default -> "часов";
+        };
+        return hoursCount > 0 ? "Осталось " + hoursCount + " " + hourName : "Осталось меньше часа";
+    }
+}
