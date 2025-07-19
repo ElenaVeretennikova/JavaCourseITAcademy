cbda9d7a9693667d29bd7dff77d2092e738befce
Elena <Lena@MacBook-Pro-.local>
2025-07-19 23:14:45 +0300

    credit03

--- src/Credit/Unit03Ex03.java
+++ src/Credit/Unit03Ex03.java
@@ -11,10 +11,15 @@ public class Unit03Ex03 {
     }
 
     public static String getHoursCount(int secCount) {
+
+        if (secCount == 0) {
+            return "Рабочий день закончился";
+        }
+
         int hoursCount = secCount / 3600;
         String hourName = switch (hoursCount) {
             case 1 -> "час";
-            case 2,3,4 -> "часа";
+            case 2, 3, 4 -> "часа";
             default -> "часов";
         };
         return hoursCount > 0 ? "Осталось " + hoursCount + " " + hourName : "Осталось меньше часа";
