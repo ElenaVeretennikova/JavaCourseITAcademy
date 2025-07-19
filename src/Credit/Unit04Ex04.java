31aa10590d744fac8efe4deadbee064f1b58288e
Elena <Lena@MacBook-Pro-.local>
2025-07-19 23:15:34 +0300

    credit04 fix

--- src/Credit/Unit04Ex04.java
+++ src/Credit/Unit04Ex04.java
@@ -5,13 +5,7 @@ public class Unit04Ex04 {
         int count = 0;
         for (int i = 1; i <= 99999; i++) {
             for (int j = i; j > 0; j /= 10) {
-                if (j % 10 == 4 && j % 100 == 13) {
-                    count++;
-                    break;
-                } else if (j % 10 == 4 && j % 100 != 13) {
-                    count++;
-                    break;
-                } else if (j % 10 != 4 && j % 100 == 13) {
+                if ((j % 10 == 4 && j % 100 == 13)||(j % 10 == 4 && j % 100 != 13)||(j % 10 != 4 && j % 100 == 13)){
                     count++;
                     break;
                 }
